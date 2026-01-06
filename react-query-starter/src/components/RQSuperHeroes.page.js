a23ef6df917fe0b7dfa536b82411c0bd9fb5e8f2
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2026-01-06 08:52:24 -0500

    Add refetchInterval

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -3,20 +3,23 @@ import { useQuery } from 'react-query';
 
 export const RQSuperHeroesPage = () => {
   const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
-  const { isLoading, data, isError, error, isFetching } = useQuery({
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery({
     queryKey: ['super-heroes'],
     queryFn: fetchSuperHeroes,
-    cacheTime: 5000, // Default: 5min,The number of secs, it will hold the data before it's Garbage collected
-    staleTime: 3000, // Default: 0, The number of seconds the data is fresh before the data becomes stale
+    refetchInterval: 3000,
+
+    // cacheTime: 5000, // Default: 5min,The number of secs, it will hold the data before it's Garbage collected
+    // staleTime: 3000, // Default: 0, The number of seconds the data is fresh before the data becomes stale
+    // refetchOnMount: false, // Options: true, false, always
+    // enabled: false,
   });
 
-  console.log({ isFetching, isLoading });
-
-  if (isLoading) return <h2>Loading...</h2>;
+  if (isLoading || isFetching) return <h2>Loading...</h2>;
   if (isError) return <h2>{error.message}</h2>;
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch heroes</button>
       <div>
         {data?.data.map((hero) => {
           return <div key={hero.id}>{hero.name}</div>;
